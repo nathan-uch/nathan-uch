6eb2f2fc6a9e891ea10761bfc1e8c53959b63367
Nathan Huang <nuchuang94@gmail.com>
2023-09-17 13:43:59 -0700

    First commit; created app with packages and dependencies

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar'
+
+export default function App() {
+
+  return (
+    <>
+      <Navbar />
+      <main id="main">
+        <div class="text-center">TEST</div>
+      </main>
+    </>
+  );
+}
---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,8 @@
+export default function App() {
+
+    return (
+      <nav id="main" class="bg-red-50">
+        <div>Navbar</div>
+      </nav>
+    );
+  }
