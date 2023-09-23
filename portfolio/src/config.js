4bd1cb7839f2d35b62a2a5aff5b8410d78034e55
Nathan Huang <nuchuang94@gmail.com>
2023-09-23 11:44:41 -0700

    Added tech stack config

--- portfolio/src/config.js
+++ portfolio/src/config.js
@@ -5,6 +5,72 @@ export const techStack = [
         icon1x: require('./icons/javascript1x.svg').default,
         icon2x: require('./icons/javascript2x.svg').default,
     },
+    {
+        name: "TypeScript",
+        icon: require('./icons/typescript.svg').default,
+        icon1x: require('./icons/typescript1x.svg').default,
+        icon2x: require('./icons/typescript2x.svg').default,
+    },
+    {
+        name: "React.js",
+        icon: require('./icons/react.svg').default,
+        icon1x: require('./icons/react1x.svg').default,
+        icon2x: require('./icons/react2x.svg').default,
+    },
+    {
+        name: "Next.js",
+        icon: require('./icons/nextjs.svg').default,
+        icon1x: require('./icons/nextjs1x.svg').default,
+        icon2x: require('./icons/nextjs2x.svg').default,
+    },
+    {
+        name: "Node.js",
+        icon: require('./icons/node.svg').default,
+        icon1x: require('./icons/node1x.svg').default,
+        icon2x: require('./icons/node2x.svg').default,
+    },
+    {
+        name: "Express.js",
+        icon: require('./icons/express.svg').default,
+        icon1x: require('./icons/express1x.svg').default,
+        icon2x: require('./icons/express2x.svg').default,
+    },
+    {
+        name: "PostgreSQL",
+        icon: require('./icons/postgresql.svg').default,
+        icon1x: require('./icons/postgresql1x.svg').default,
+        icon2x: require('./icons/postgresql2x.svg').default,
+    },
+    {
+        name: "HTML",
+        icon: require('./icons/html.svg').default,
+        icon1x: require('./icons/html1x.svg').default,
+        icon2x: require('./icons/html2x.svg').default,
+    },
+    {
+        name: "CSS",
+        icon: require('./icons/css.svg').default,
+        icon1x: require('./icons/css1x.svg').default,
+        icon2x: require('./icons/css2x.svg').default,
+    },
+    {
+        name: "GraphQL",
+        icon: require('./icons/graphql.svg').default,
+        icon1x: require('./icons/graphql1x.svg').default,
+        icon2x: require('./icons/graphql2x.svg').default,
+    },
+    {
+        name: "Tailwind",
+        icon: require('./icons/tailwind.svg').default,
+        icon1x: require('./icons/tailwind1x.svg').default,
+        icon2x: require('./icons/tailwind2x.svg').default,
+    },
+    {
+        name: "Bootstrap",
+        icon: require('./icons/bootstrap.svg').default,
+        icon1x: require('./icons/bootstrap1x.svg').default,
+        icon2x: require('./icons/bootstrap2x.svg').default,
+    },
 ]
 
 export const otherTools = [
